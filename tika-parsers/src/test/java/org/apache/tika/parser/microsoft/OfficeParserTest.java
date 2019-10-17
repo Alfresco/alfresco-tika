928e0620c132f2edf44eeeb3bda8117e8d5abfd6
tiagos <tiagosalvado10@gmail.com>
2019-10-17 18:53:39 +0100

    [REPO-4719] [Apache POI Bug 61295] Changed tika-parsers pom file to use apache-poi 3.10.1-20191015-alfresco-patched. Added unit test and test file.

--- tika-parsers/src/test/java/org/apache/tika/parser/microsoft/OfficeParserTest.java
+++ tika-parsers/src/test/java/org/apache/tika/parser/microsoft/OfficeParserTest.java
@@ -20,28 +20,75 @@ import static junit.framework.Assert.assertTrue;
 
 import java.io.InputStream;
 
+import org.apache.poi.poifs.filesystem.DirectoryNode;
+import org.apache.poi.poifs.filesystem.NPOIFSFileSystem;
 import org.apache.tika.TikaTest;
+import org.apache.tika.io.CloseShieldInputStream;
 import org.apache.tika.io.TikaInputStream;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.parser.Parser;
 import org.apache.tika.parser.microsoft.ooxml.OOXMLParserTest;
 import org.junit.Test;
 
-
-
 public class OfficeParserTest extends TikaTest {
 
-  @Test
-  public void parseOfficeWord() throws Exception {
-    Metadata metadata = new Metadata();
-    Parser parser = new OfficeParser();
+	@Test
+	public void parseOfficeWord() throws Exception {
+		Metadata metadata = new Metadata();
+		Parser parser = new OfficeParser();
 
-    String xml = getXML(getTestDocument("test.doc"), parser, metadata).xml;
+		String xml = getXML(getTestDocument("test.doc"), parser, metadata).xml;
 
-    assertTrue(xml.contains("test"));
-  }
+		assertTrue(xml.contains("test"));
+	}
 
-  private InputStream getTestDocument(String name) {
-    return TikaInputStream.get(OOXMLParserTest.class.getResourceAsStream("/test-documents/" + name));
-}
+	@Test
+	public void testPoiBug61295() throws Exception {
+
+		boolean passed = false;
+		DirectoryNode root;
+		InputStream stream = getTestDocument("61295.tmp");
+		
+		try {
+			TikaInputStream tstream = TikaInputStream.cast(stream);
+			
+			if (tstream == null) {
+				root = new NPOIFSFileSystem(new CloseShieldInputStream(stream)).getRoot();
+			} else {
+				final Object container = tstream.getOpenContainer();
+				if (container instanceof NPOIFSFileSystem) {
+					root = ((NPOIFSFileSystem) container).getRoot();
+				} else if (container instanceof DirectoryNode) {
+					root = (DirectoryNode) container;
+				} else if (tstream.hasFile()) {
+					root = new NPOIFSFileSystem(tstream.getFileChannel()).getRoot();
+				} else {
+					root = new NPOIFSFileSystem(new CloseShieldInputStream(tstream)).getRoot();
+				}
+			}
+
+			Metadata metadata = new Metadata();
+			new SummaryExtractor(metadata).parseSummaries(root);
+			
+			passed = true;
+		} catch (ArrayIndexOutOfBoundsException e) {
+			passed = true;
+		} catch (Exception exception) {
+			passed = false;
+		} finally {
+			if (stream != null) {
+				try {
+					stream.close();
+				} catch (Exception e2) {
+					stream = null;
+				}
+			}
+		}
+
+		assertTrue("An exception that is not ArrayIndexOutOfBoundsException has been thrown", passed);
+	}
+
+	private InputStream getTestDocument(String name) {
+		return TikaInputStream.get(OOXMLParserTest.class.getResourceAsStream("/test-documents/" + name));
+	}
 }
